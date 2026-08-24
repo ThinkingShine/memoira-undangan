23da9bd9bb22a80c398038eef82d0003a0e03bdc
ThinkingShine <fikri230768@gmail.com>
2026-08-25 06:32:55 +0700

    Update EventDetails.tsx

--- src/app/components/EventDetails.tsx
+++ src/app/components/EventDetails.tsx
@@ -122,7 +122,7 @@ export function EventDetails() {
           time="15.30 WIB — selesai"
           venue="Rumah Mempelai Perempuan"
           address="Perumahan Mangunjaya 1, Tambun, Bekasi, Jawa Barat"
-          mapsUrl="https://maps.app.goo.gl/191kRJtx86i5YMvv9"        
+          mapsUrl="https://maps.app.goo.gl/6hRnLDYC7eybY2tv5"        
         />
         <EventCard
           heading="Ngunduh Mantu"
